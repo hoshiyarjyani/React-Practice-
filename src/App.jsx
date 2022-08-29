61335d72a8a5bd430285f2c9aa5bcb1cef2b9bdc
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-08-29 23:37:15 +0530

    hook function in react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const App = () => {
+
+    // hook function in react
+    
+const [count, setCount] = useState(1);
+const IncNum = () => {
+    setCount(count+1);
+};
+return(
+<>
+<h1>{count}</h1>
+<button onClick={IncNum}>click me</button>
+</>)};
+export default App;
